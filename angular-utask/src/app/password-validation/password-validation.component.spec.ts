2ff98132ffa9b6ad61b065fa0bf3eec66ff4bff7
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-02-02 17:50:16 +0400

    created-password-validation component

---
+++ angular-utask/src/app/password-validation/password-validation.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PasswordValidationComponent } from './password-validation.component';
+
+describe('PasswordValidationComponent', () => {
+  let component: PasswordValidationComponent;
+  let fixture: ComponentFixture<PasswordValidationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PasswordValidationComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PasswordValidationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-utask/src/app/password-validation/password-validation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-validation',
+  templateUrl: './password-validation.component.html',
+  // styleUrls: ['./password-validation.component.css'],
+})
+export class PasswordValidationComponent {}
